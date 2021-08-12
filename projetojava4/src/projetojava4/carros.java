5e022fa14c0d8ae08ba8c68cf9d429f40b8e779a
User <User@DESKTOP-K0C2U5T>
2021-08-12 14:51:59 -0300

    commit

--- projetojava4/src/projetojava4/carros.java
+++ projetojava4/src/projetojava4/carros.java
@@ -1,5 +1,26 @@
 package projetojava4;
 
 public class carros {
+	
+	
+	private String Modelo;
+
+	private String Cor ;
+
+	public String getModelo() {
+		return Modelo;
+	}
+
+	public void setModelo(String modelo) {
+		Modelo = modelo;
+	}
+
+	public String getCor() {
+		return Cor;
+	}
+
+	public void setCor(String cor) {
+		Cor = cor;
+	}
 
 }
